df1c2bd082a5ab624ed5cd06143a1494ccde7f64
tommaso-mandorino <tommasomandorino@outlook.it>
2025-07-22 17:08:32 +0200

    feat: finish milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+// useEffect importing
+import { useEffect } from 'react';
+
+// Endpoints importing
+import endopoints from './constants/endpoints.js';
+
+// App component
 function App() {
+
+  // Fetch data useEffect binding
+  useEffect(() => fetchData, []);
+
+  // Fetch data function
+  function fetchData() {
+
+    // Start fetching
+    fetch(endopoints.actors)
+
+      // Start converting response to JSON
+      .then(response => response.json())
+      
+      // Use data
+      .then(data => {
+
+        // Log data
+        console.log(data);
+
+      });
+
+  }
 
   return (
 
---
+++ src/constants/endpoints.js
@@ -0,0 +1,9 @@
+const endpoints = {
+
+    actors: 'https://lanciweb.github.io/demo/api/actors/',
+
+    actresses: 'https://lanciweb.github.io/demo/api/actresses/'
+
+}
+
+export default endpoints;
